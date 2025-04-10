247da3598cf19fa1058f1dadcf56c7fd0f501f7a
Gavin Fuqua <gavin.fuqua@icloud.com>
2025-04-10 11:59:58 -0400

    getCanvasToken Function

--- bot/databaseFunctions.py
+++ bot/databaseFunctions.py
@@ -3,6 +3,12 @@ import mysql.connector
 # This script will contain functions the bot will use for database calls.
 
 
+async def getCanvasToken(discordID: int) -> int:
+    # Get canvas token from discord id then return
+    canvasToken = 123456789
+    return canvasToken
+
+
 async def changeNotificationSetting(
     settingName: str, settingValue: int, discordID: int
 ):
